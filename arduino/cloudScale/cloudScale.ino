b030e25cd9fb3cd3ab29cf1710d38541ee08fe23
tim-fan <tim.fanselow@gmail.com>
2016-06-20 13:14:07 +1200

    Code clean up in cloudScale.ino

--- arduino/cloudScale/cloudScale.ino
+++ arduino/cloudScale/cloudScale.ino
@@ -31,7 +31,6 @@ PinInterfaceDirect pinInterface7(A7, scaleFactorToVolts);
 
 //digits array, representing 4 seven-segment digits 
 //on the scale's LCD
-
 SevenSeg digits[4] {
     SevenSeg(&pinInterface7, &pinInterface6), //decimal point
     SevenSeg(&pinInterface1, &pinInterface0), // ones
@@ -53,7 +52,6 @@ SoftwareSerial softSerial(pinRx, pinTx); // RX, TX
 const int pinBootMode = 5; //aka pin GPIO_0
 const int pinRst = 6;   
 
-int ledState = false;
 char terminator = char(0);
 
 void checkIncommingEspSerial()
@@ -61,11 +59,8 @@ void checkIncommingEspSerial()
   //checks for serial data from the ESP.
   //if there is data available, it is 
   //printed to the hardware serial.
-  //It is assumed messages are sent from the ESP 
-  //with a null char terminator.
+  
   char received;
-//  Serial.println("ESP serial check");
-//  String message = "";
   if (softSerial.available())
   {
     Serial.print("From ESP8266: ");
@@ -76,42 +71,22 @@ void checkIncommingEspSerial()
     Serial.print(received);
   }
   Serial.println("");
-//    received = softSerial.read();
-//    if (received == '\0')
-//    {
-//      Serial.println(message);
-//      message = String();  
-//    }
-//    
-//    message.concat(char(softSerial.read()));    
-//    Serial.println("ESP serial read");
-//    softSerial.setTimeout(1000);
-//    received = softSerial.readStringUntil('Z');
-//    Serial.println("ESP serial read complete");
-//    Serial.println(received);
-//    if (!received.endsWith(String(terminator)))
-//    {
-//      Serial.println("Incomplete message: no terminator received")  ;
-//    }
-//    delay(100);
-//  }
-//  Serial.println(message + String(" (unterminated)"));
-//  Serial.println("ESP serial check complete");
-//  return received;
 }
 
 void setup() {
 
-  //debug info goes to Serial, 
-  //weight values for logging go to ESP8266 via softSerial
-  
+  //start serial (software and hardware)
   Serial.begin(9600);
   softSerial.begin(9600);
+  
+  //run initialisation functions for the scale
   scale.init();  
-  pinMode(LED_BUILTIN, OUTPUT);
-
+  
+  //set pin modes
   pinMode(pinRst, OUTPUT);
   pinMode(pinBootMode, OUTPUT);
+  
+  //reset ESP8266
   digitalWrite(pinBootMode, HIGH);
   delay(500); //I don't really know how long these delays should be...
   digitalWrite(pinRst, LOW);
@@ -122,36 +97,12 @@ void setup() {
 
 
 void loop() {
-  ledState = !ledState;
-  
-  //digitalWrite(LED_BUILTIN, analogRead(A0)>10);   
-  //Serial.println(analogRead(A0));
-  
-//  //Voltage level testing
-//  int vals[64];
-//  pinInterface0.readPinRoundedVolts();
-//  while(pinInterface0.readPinRoundedVolts() > 0) {}
-//  while(pinInterface0.readPinRoundedVolts() <= 0)  {}
-//  delay(1);
-//  unsigned long startTime = micros();
-//  for (int i = 0; i < 64; i++){
-//    vals[i] = pinInterface0.readPinRoundedVolts();
-////    vals[i] = pinInterface0.readPin();
-//    while (micros() - startTime < (i+1)*1000){
-//      //busy waiting
-//    }
-//  }
-//  Serial.println(-1);
-//  for (int i = 0; i < 64; i++){
-//    Serial.println(vals[i]);
-//  }
-//  delay(1000);
-//  Serial.println("End of sequence");
-  
-//
+  //read weight from scale
   float weight = scale.readStableWeight();
   Serial.println(weight);
   bool weightIsValid = weight >= 0;
+  
+  //if valid weight received, send over soft-serial to ESP8266 for logging
   if (weightIsValid){
     Serial.println("Got stable weight!");
     Serial.println(weight);
@@ -160,11 +111,9 @@ void loop() {
     softSerial.print(weight);
     softSerial.print(terminator);
   }
-
-  checkIncommingEspSerial();
-   
-  delay(200);  
   
+  checkIncommingEspSerial();
+  delay(200);
 }
 
 
